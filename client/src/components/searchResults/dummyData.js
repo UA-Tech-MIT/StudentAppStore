d47e77bb06bb45b338fd7baeb5424807ea1c6a36
Yaateh Richardson <yaatehr@mit.edu>
2018-08-03 01:27:28 -0400

    Import search results changes and update schema for app rating

---
+++ client/src/components/searchResults/dummyData.js
@@ -0,0 +1,76 @@
+export default {
+    "data": {
+      "allApps": [
+        {
+          "id": "577120fd-6f5a-4aa2-b936-768e1fe008cb",
+          "appNo": 4,
+          "name": "Simonis Inc",
+          "author": "Electa Schaden",
+          "medium": "black",
+          "genre": "bypass",
+          "rating": 4.5,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "725d7f41-9fb0-4a86-8553-d86a94d6ebc0",
+          "appNo": 3,
+          "name": "Nitzsche, Ernser and Kuhic",
+          "author": "Reggie Ferry",
+          "medium": "payment",
+          "genre": "withdrawal",
+          "rating": 2,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "7458e21d-f581-456b-88b5-8123135609fc",
+          "appNo": 1,
+          "name": "Predovic Inc",
+          "author": "Katelynn Schmitt",
+          "medium": "primary",
+          "genre": "Liberia",
+          "rating": 2.4,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "8bea0813-f42f-4ac1-86bb-14d5de1fe004",
+          "appNo": 5,
+          "name": "Veum, Hilll and Gusikowski",
+          "author": "Camilla Jerde",
+          "medium": "deposit",
+          "genre": "real-time",
+          "rating": 3.9,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "995a7c9a-d8c7-4704-9c63-bbf64d8adaa3",
+          "appNo": 7,
+          "name": "Schulist and Sons",
+          "author": "Rubye Schaefer",
+          "medium": "efficient",
+          "genre": "impactful",
+          "rating": 1.8,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "9d65dfef-6f43-4317-9f1d-4e8d9c4ace6e",
+          "appNo": 2,
+          "name": "Cummings - Bahringer",
+          "author": "Barney Conroy",
+          "medium": "deposit",
+          "genre": "Fantastic",
+          "rating": 0.2,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        },
+        {
+          "id": "f2d64c75-1837-415f-ba6d-bccd0e50cf04",
+          "appNo": 6,
+          "name": "Renner LLC",
+          "author": "Willow Reinger",
+          "medium": "Minnesota",
+          "genre": "Graphical User Interface",
+          "rating": 2.9,
+          "description": "ius in dicant maluisset euripidis.\n Ne eum vitae eirmod aliquid, duo an mazim semper suscipiantur."
+        }
+      ]
+    }
+  };
