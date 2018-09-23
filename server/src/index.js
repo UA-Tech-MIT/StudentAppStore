da3b99066e7ed0c904ad54f451c20b7c4a7cc70c
Yaateh Richardson <yaatehr@mit.edu>
2018-09-23 14:24:56 -0400

    parsing partially complete, app apge functional but not reactive, most pictures uploaded

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,8 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { schemaArray } from './schema/index';
 import resolvers from './resolvers';
 import models from './models';
+import parseFile from './parser'
+const fs = require('fs');
 
 
 const schema = makeExecutableSchema({
@@ -15,60 +17,62 @@ const schema = makeExecutableSchema({
   resolvers: { ...resolvers },
 });
 
+const file = fs.createReadStream('app-list.csv');
+parseFile(file);
 
-const graphqlEndpoint = '/graphql';
-const SECRET = 'asiodfhoi1hoi23jnl1kejd';
-const SECRET2 = 'a0y7986g786c4uetrytb705864durc';
-const addUser = async (req, res, next) => {
-  const token = req.headers['x-token'];
-  if (token) {
-    try { 
-      const { user } = jwt.verify(token, SECRET);
-      req.user = user;
-    } catch (err) {
-      const refreshToken = req.headers['x-refresh-token'];
-      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
-      if (newTokens.token && newTokens.refreshToken) {
-        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
-        res.set('x-token', newTokens.token);
-        res.set('x-refresh-token', newTokens.refreshToken);
-      }
-      req.user = newTokens.user;
-    }
-  }
-  next();
-};
+// const graphqlEndpoint = '/graphql';
+// const SECRET = 'asiodfhoi1hoi23jnl1kejd';
+// const SECRET2 = 'a0y7986g786c4uetrytb705864durc';
+// const addUser = async (req, res, next) => {
+//   const token = req.headers['x-token'];
+//   if (token) {
+//     try { 
+//       const { user } = jwt.verify(token, SECRET);
+//       req.user = user;
+//     } catch (err) {
+//       const refreshToken = req.headers['x-refresh-token'];
+//       const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+//       if (newTokens.token && newTokens.refreshToken) {
+//         res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+//         res.set('x-token', newTokens.token);
+//         res.set('x-refresh-token', newTokens.refreshToken);
+//       }
+//       req.user = newTokens.user;
+//     }
+//   }
+//   next();
+// };
 
-console.log('Server is running  ');
+// console.log('Server is running  ');
 
-const APP_PORT = 8080;
+// const APP_PORT = 8080;
 
-const app = Express();
-app.use(cors('*'));
-app.use(addUser);
+// const app = Express();
+// app.use(cors('*'));
+// app.use(addUser);
 
 
-app.use(graphqlEndpoint, bodyParser.json(), GraphHTTP( async (req, res) => ({
-  schema: schema,
-  pretty: true,
-  // cacheControl: true,
-  graphiql: true,
-  context: {
-    models,
-    user: req.user,
-    SECRET,
-    SECRET2,
-  },
-})));
+// app.use(graphqlEndpoint, bodyParser.json(), GraphHTTP( async (req, res) => ({
+//   schema: schema,
+//   pretty: true,
+//   // cacheControl: true,
+//   graphiql: true,
+//   context: {
+//     models,
+//     user: req.user,
+//     SECRET,
+//     SECRET2,
+//   },
+// })));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-setTimeout(() => {
-  initDatabase();
-}, 4000);
+// setTimeout(() => {
+//   initDatabase();
+// }, 4000);
 
-app.listen(APP_PORT, () => {
-  console.log('DB Listening on port 8080\n');
-});
+// app.listen(APP_PORT, () => {
+//   console.log('DB Listening on port 8080\n');
+// });
