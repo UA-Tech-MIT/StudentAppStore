5725890864f503fb3f33b9aa13b76c19d13d797f
Yaateh Richardson <yaatehr@mit.edu>
2019-04-21 14:34:42 -0400

    minor upudate

--- server/src/models/appTag.js
+++ server/src/models/appTag.js
@@ -1,29 +1,24 @@
 export default (Conn, Sequelize) => {
     
 const AppTag = Conn.define('app_tag', {
-      // appHash : {
-      //   type: Sequelize.UUID,
-      //   foreign: true,
-      // },
-      // name: {
-      //   type: Sequelize.STRING,
-      //   primaryKey: true,
-      // }
-
-
-      // tag_id: {
-      //   type: Sequelize.INTEGER,
-      //   unique: 'app_tag_taggable'
-      // },
-      // taggable: {
-      //   type: Sequelize.STRING,
-      //   unique: 'app_tag_taggable'
-      // },
-      // taggable_id: {
-      //   type: Sequelize.STRING,
-      //   unique: 'app_tag_taggable',
-      //   references: null
-      // }
+      id : {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      tag_id: {
+        type: Sequelize.INTEGER,
+        unique: 'app_tag_taggable'
+      },
+      taggable: {
+        type: Sequelize.STRING,
+        unique: 'app_tag_taggable'
+      },
+      taggable_id: {
+        type: Sequelize.INTEGER,
+        unique: 'app_tag_taggable',
+        references: null
+      }
     });
   return AppTag;
 };
